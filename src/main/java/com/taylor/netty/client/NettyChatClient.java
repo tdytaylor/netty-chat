b923df582ef351188e564eac75e3dee63f7b1d24
tdytaylor <949722633@qq.com>
2018-10-06 00:28:18 +0800

    增加客户端重连机制
    Signed-off-by: tdytaylor <949722633@qq.com>

--- src/main/java/com/taylor/netty/client/NettyChatClient.java
+++ src/main/java/com/taylor/netty/client/NettyChatClient.java
@@ -1,14 +1,13 @@
 package com.taylor.netty.client;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -17,7 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NettyChatClient {
 
-  public static void main(String[] args) throws InterruptedException {
+  private static final int MAX_RETRY = 5;
+
+  public static void main(String[] args) {
     NioEventLoopGroup worker = new NioEventLoopGroup(1);
     Bootstrap bootstrap = new Bootstrap();
     bootstrap.group(worker)
@@ -26,19 +27,53 @@ public class NettyChatClient {
         .handler(new ChannelInitializer<SocketChannel>() {
           @Override
           protected void initChannel(SocketChannel ch) throws Exception {
-            //ch.pipeline().addLast(new ClientChannelHandler());
+            ch.pipeline().addLast(new ClientChannelHandler());
           }
         });
 
-    ChannelFuture channelFuture = bootstrap.connect("localhost", 8080).sync()
+    /*ChannelFuture channelFuture = bootstrap.connect("localhost", 8080).sync()
         .addListener(future -> {
           if (future.isSuccess()) {
             log.info("连接服务端成功！");
+          } else {
+            int i = 0;
+            while (i < MAX_RETRY) {
+              bootstrap.connect("localhost", 8080).sync().addListener(future1 -> {
+                    if (future1.isSuccess()) {
+                      return;
+                    }
+                  }
+              );
+            }
           }
-        });
+        });*/
+    connect(bootstrap, "localhost", 8080, MAX_RETRY);
 
-    ByteBuf buf = channelFuture.channel().alloc().buffer(20)
+    /*ByteBuf buf = channelFuture.channel().alloc().buffer(20)
         .writeBytes("你好，服务器。".getBytes(Charset.forName("UTF-8")));
-    channelFuture.channel().writeAndFlush(buf);
+    channelFuture.channel().writeAndFlush(buf);*/
+  }
+
+  private static void connect(Bootstrap bootstrap, String hostname, int port, int retry) {
+    bootstrap.connect(hostname, port).addListener(future -> {
+      if (future.isSuccess()) {
+        log.info("连接服务器成功。服务器地址：{}，端口：{}", hostname, port);
+      } else if (retry == 0) {
+        log.info("连接服务器失败。已尝试重新连接{}次。", MAX_RETRY);
+      } else {
+///        log.info("尝试重新连接服务器。");
+//        connect(bootstrap, hostname, port, MAX_RETRY - 1);
+
+        // 第几次重连
+        int order = (MAX_RETRY - retry) + 1;
+        // 本次重连的间隔
+        int delay = 1 << order;
+        System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+        // 使用NioEventLoopGroup执行重连，不占用主线程
+        bootstrap.config().group()
+            .schedule(() -> connect(bootstrap, hostname, port, retry - 1), delay, TimeUnit
+                .SECONDS);
+      }
+    });
   }
 }
